40b8a5973214274a489b2401dc093e1502d42891
oguzhanorhan <ozy.orhan@gmail.com>
2019-05-07 16:22:56 +0300

    Webview zooom control added for ios
    Webview zooom control added for ios

---
+++ XF_WebviewPDF.iOS/CustomRenderers/PdfWebViewRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XF_WebviewPDF.iOS.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(WebView), typeof(PdfWebViewRenderer))]
+namespace XF_WebviewPDF.iOS.CustomRenderers
+{
+    public class PdfWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (NativeView != null && e.NewElement != null)
+            {
+                var pdfControl = NativeView as UIWebView;
+
+                if (pdfControl == null)
+                    return;
+
+                pdfControl.ScalesPageToFit = true;
+            }
+        }
+    }
+}
